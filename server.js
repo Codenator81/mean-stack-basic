b18c9dd4ae8ef6e66256b2f334113b2ce9d590f5
Codenator81 <codenator81@gmail.com>
2016-05-10 16:37:38 +0300

    Create express app with session and views

---
+++ server.js
@@ -0,0 +1,7 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var express = require('./config/express');
+var app = express();
+app.listen(3000);
+module.exports = app;
+console.log('Server running at http://localhost:3000/');
