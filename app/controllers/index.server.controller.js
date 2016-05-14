2de3210fba5a9bbac2d22e63f00ba9aea96fbb06
Codenator <codenator81@gmail.com>
2016-05-14 13:16:17 +0300

    Finish sign in and sign out with local strategy

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -6,6 +6,7 @@ exports.render = function (req, res) {
     req.session.lastVisit = new Date();
 
     res.render('index', {
-        title: 'Hello World EJS'
+        title: 'Hello World EJS',
+        userFullName: req.user ? req.user.fullName : ''
     })
 };
